91df9e78ce4a471cdfc9ed419b6a77e697224f04
anji0114 <anji.tnk.140114@gmail.com>
2022-04-21 05:47:10 +0900

    基本ファイルの作成

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* Twitterアイコン */}
+
+      {/* sidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+};
